b27e0c204036ac2d311f0644a8ed48fe18636717
azhes <graystonea@gmail.com>
2022-11-16 18:52:32 -0800

    audit debugging

--- Audit/app.py
+++ Audit/app.py
@@ -43,8 +43,10 @@ def get_posted_trade(index):
     logger.info(f'Retrieving posted trade at {index}')
     try:
         msg = consumer.consume(index)
+        
         msg_str = msg.value.decode('utf-8')
         msg_json = json.loads(msg_str)
+        logger.debug(f'Requested message: {json.loads(msg_str)}')
 
         # Find the event at the index you want and return code 200
         return msg_json, 200
